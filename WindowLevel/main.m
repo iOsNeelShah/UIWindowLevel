0308f6eb537a0294f4a5ac4e65631d8ddbeed2b5
Neel Shah <neels@SA-MAC-007.local>
2014-05-15 14:06:56 +0530

    Initial Commit

---
+++ WindowLevel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WindowLevel
+//
+//  Created by Neel Shah on 15/05/14.
+//  Copyright (c) 2014 com.solutionanalysts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
